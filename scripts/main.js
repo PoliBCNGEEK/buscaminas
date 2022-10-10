804f31b7514a18bcd7f47a1e24c75aa10b797463
poliernus <polmeca@gmail.com>
2022-10-10 11:34:14 +0200

    cambios

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,4 @@
-const { findLast } = require("lodash");
 
-smile();
 function gameData(){
     var game = {}
     game.state = "";
@@ -8,12 +6,34 @@ function gameData(){
     game.height = "8";
     game.width = "8";
     game.numMines = "10";
-
     return game;
 }
 const obj = gameData();
 
 gameData();
+smile();
+const mineField = [];
+minefieldCreation();
+
+//En el apartado de mine:false crear despues una function que los ponga random contando cada numero de random hasta que te quedes sin poner ninguna mina. !!!!!!
+function minefieldCreation(){
+    for (let i = 0; i < obj.height; i++){
+        mineField.push([])
+        console.log("----------"+i)
+            for (let j = 0; j < obj.width; j++){
+                console.log(j);
+                mineField[i].push({
+                    mine:false,
+                    hidden:true,
+                    num:null
+                })
+            }
+    }
+    if (mineField[0,1].mine == false){
+        console.log("mineField[0,1]")
+
+    }
+}
 
 /*
 minefield[]
@@ -35,7 +55,6 @@ minefield.push({
 mineField[0,2].state
 
 */
-
 function  smile(){
     var src = document.getElementById("smile");
     var img = document.createElement("img");
